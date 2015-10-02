9c79dd9db86b1b899026bfe4acdb79e2d4a8c8a5
Oliver Müller <oliver.mueller@shopgate.com>
2015-10-02 08:15:17 +0200

    Update Shopgate Plugin to version 2.9.51.

---
+++ nl.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oliver
+ * Date: 02.10.15
+ * Time: 08:08
+ */
--- shopgate.php
+++ shopgate.php
@@ -29,7 +29,7 @@ if (!defined('_PS_VERSION_')) {
 /**
  * define shopgate version
  */
-define('SHOPGATE_PLUGIN_VERSION', '2.9.50');
+define('SHOPGATE_PLUGIN_VERSION', '2.9.51');
 
 /**
  * define module dir
@@ -148,7 +148,7 @@ class ShopGate extends PaymentModule
             $this->tab = 'mobile';
         }
 
-        $this->version = '2.9.50';
+        $this->version = '2.9.51';
         $this->author = 'Shopgate';
         $this->module_key = '';
 
